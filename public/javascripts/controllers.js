38fbbdd1d4b5840240275210549541307c1c417f
Will Moritz <will@crowdfavorite.com>
2013-11-03 04:51:44 -0700

    working rendering of emails and names with custom html for autocomplete

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -10,17 +10,38 @@ angular.module('emit.controllers', []).
       url: '/user/gcontacts'
     }).
     success(function (data, status, headers, config) {
-      console.log(data);
-      var array = [];
-      for(var i=0; i < data.length; i++){
-        // http://stackoverflow.com/questions/16309940/jquery-autocomplete-vs-gmail-autocomplete
-        array.push(data[i].name + data[i].email + '</span>');
-      // array.push('<span class="name">' + data[i].name + '</span>' + '<span class="email">' data[i].email + '</span>');
-      }
 
+      // Set up autocomplete for email form
       $("#contacts").autocomplete({
-        source: array
-      });
+        source: function(req, res) {
+          // http://jsbin.com/utojoh/2/edit
+            // console.log(data);
+
+            var array = [];
+            var arrayIndexes = [];
+            var resultsArray = [];
+            for(var i=0; i < data.length; i++){
+              array.push(data[i].name + ' ' + data[i].email);
+            }
+
+            var re = $.ui.autocomplete.escapeRegex(req.term);
+            var matcher = new RegExp( "\\b" + re, "i" );
+            var a = $.grep( array, function(item,index){
+                if (matcher.test(item)){
+                  resultsArray.push(data[(array.indexOf(item))]);
+                  return true;
+                }
+            });
+            console.log(resultsArray);
+            res( resultsArray );
+        },
+        matchContains: true,
+        delay: 0
+      }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+      return $( "<li>" )
+        .append( '<span class="google-contact-name">'+ item.name + '</span><br /><span class="google-contact-email">' + item.email + "</span>" )
+        .appendTo( ul );
+      };
 
       $scope.contacts = data;
     }).
